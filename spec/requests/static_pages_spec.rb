332ada7745bfb34fc10221723508704c3b1f4a07
sei t <eisei@tsMacBook-Air.local>
2023-03-12 17:28:43 +0900

    update static_pages_spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
+  let(:base_title) { 'せるまね' }
   describe "GET root" do
     it "レスポンスが正常であること" do
       get root_path
       expect(response).to have_http_status(:success)
+    end
+    it "タイトルが正常に表示されていること" do
+      get root_path
+      expect(response.body).to include("Home | #{base_title}")
     end
   end
 end
