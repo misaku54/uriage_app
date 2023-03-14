5a9bcce7e27728d2c82c43195bba87ddf930bd58
sei t <eisei@tsMacBook-Air.local>
2023-03-14 21:49:58 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  root 'static_pages#home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "static_pages#home"
+  get  "/signup",  to: "users#new"
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /new" do
-    it "returns http success" do
-      get "/users/new"
+    it "レスポンスが正常であること" do
+      get signup_path
       expect(response).to have_http_status(:success)
     end
   end
