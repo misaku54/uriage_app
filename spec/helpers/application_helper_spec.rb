9f1c5dda5a30c7880769d304de48cdd6fa777d92
sei t <eisei@tsMacBook-Air.local>
2023-03-14 22:08:19 +0900

    Mkdir helpers Add application_helper

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe 'full_title' do
+    let(:base_title) { 'せるまね' }
+    context '引数を渡した場合' do
+      it '引数の文字列とベースタイトルが返ること' do
+        expect(full_title('Page Title')).to eq "Page Title | #{base_title}"
+      end
+    end
+    context '引数を渡さなかった場合' do
+      it 'ベースタイトルのみ返ること' do
+        expect(full_title()).to eq "#{base_title}"
+      end
+    end
+  end
+end
