bec2feeec48bda3e6ae3e911d49332c971ae8e52
sei t <eisei@tsMacBook-Air.local>
2023-03-16 02:44:15 +0900

    Update flash&error_message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = '新規登録に成功しました。'
-      redirect_to root_path
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
