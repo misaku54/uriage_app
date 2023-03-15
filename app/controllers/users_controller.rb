6e41be488b5f243b5ad5b49f1d0b2940aee0d95c
sei t <eisei@tsMacBook-Air.local>
2023-03-15 18:39:29 +0900

    Add users_controller create_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規登録に成功しました。'
+      redirect_to root_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+
+  private 
+    # ストロングパラメーター
+    def user_params
+      params.require(:user).permit(:name, :email, :password, 
+                                                  :password_confirmation)
+    end
 end
