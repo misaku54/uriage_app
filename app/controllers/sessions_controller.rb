84e3d79e00b04a78627456ec61661746582e2a89
sei t <eisei@tsMacBook-Air.local>
2023-03-17 01:02:11 +0900

    Add session_create_action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+    else
+      flash.now[:danger] = 'メールアドレスまたはパスワードが違います。'
+      render 'new', status: :unprocessable_entity
+    end
+  end
 end
