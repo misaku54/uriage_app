7ddc26ddfdcc78fd420ce005ad46f9eec84fb635
sei t <eisei@tsMacBook-Air.local>
2023-03-17 06:22:30 +0900

    Update _hader.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'メールアドレスまたはパスワードが違います。'
       render 'new', status: :unprocessable_entity
--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -5,5 +5,18 @@ RSpec.describe "Sessions", type: :system do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe '#create' do
+    context '無効な値の場合' do
+      it 'flashメッセージが表示されていること' do
+        visit login_path
+        fill_in 'メールアドレス', with: ''
+        fill_in 'パスワード', with: ''
+        click_button 'ログイン'
+        expect(page).to have_selector 'div.alert.alert-danger'
+
+        visit root_path
+        expect(page).to_not have_selector 'div.alert.alert-danger'
+      end
+    end
+  end
 end
