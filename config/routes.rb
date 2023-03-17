d11f5f17221831edb2fed0b5d772dfa3295c7530
sei t <eisei@tsMacBook-Air.local>
2023-03-18 01:11:25 +0900

    Update app/controllers/sessions_controller.rb destroy

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#new'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
