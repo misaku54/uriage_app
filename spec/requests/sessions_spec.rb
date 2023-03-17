3d658c921b301e3a7ff6ce43a7567be29911ead7
sei t <eisei@tsMacBook-Air.local>
2023-03-17 23:04:30 +0900

    Update spec/requests/sessions_spec.rb

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "GET /new" do
-    it "レスポンスが正常であること" do
+  describe '#new' do
+    it 'レスポンスが正常であること' do
       get login_path
       expect(response).to have_http_status(:success)
+    end
+  end
+  
+  describe '#create' do
+    context 'メールアドレスは正しいが、パスワードが誤っていた場合' do
+      before do
+        user = FactoryBot.create(:user)
+        session_params = { email: user.email, password: 'invalid'}
+        post login_path, params: { session: session_params }
+      end
+
+      it 'ログイン画面を再表示すること' do
+        expect(response.body).to include('ログイン')
+      end
     end
   end
 end
