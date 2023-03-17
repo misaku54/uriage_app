230fa2efc0ea4bf6577c6b591c7e8e26a7ab880a
sei t <eisei@tsMacBook-Air.local>
2023-03-17 20:02:34 +0900

    Add javascript toggle_menu

---
+++ app/javascript/custom/menu.js
@@ -0,0 +1,8 @@
+document.addEventListener("turbo:load", function() {
+  let hamburger = document.querySelector("#hamburger");
+  hamburger.addEventListener("click", function(event) {
+    event.preventDefault();
+    let menu = document.querySelector("#navbar-menu");
+    menu.classList.toggle("collapse");
+  });
+});
