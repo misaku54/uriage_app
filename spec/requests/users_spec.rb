db392c85836781d605cb5e375ef192a9f5ed700e
misaku54 <eisei0504@outlook.jp>
2023-03-18 19:06:46 +0900

    Add spec/requests/users_spec.rb #update

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -34,21 +34,53 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
-  # describe '#update' do
-  #   let!(:user) { FactoryBot.create(:user) }
+  describe '#update' do
+    let!(:user) { FactoryBot.create(:user) }
 
-  #   context '無効な値の場合' do
-  #     it '更新されないこと' do
-  #       user_params = FactoryBot.attributes_for(:invalid_user)
-  #       patch user_path(user), params: { user: user_params }
-  #       user.reload
-  #       aggregate_failures do
-  #         expect(user.name).to_not eq user_params[:name]
-  #         expect(user.email).to_not eq user_params[:email]
-  #         expect(user.password).to_not eq user_params[:password]
-  #         expect(user.password_confirmation).to_not eq user_params[:password_confirmation]
-  #       end
-  #     end
-  #   end
-  # end
+    context '無効な値の場合' do
+      before do
+        @user_params = FactoryBot.attributes_for(:invalid_user)
+        patch user_path(user), params: { user: @user_params }
+      end
+      it '更新されないこと' do
+        user.reload
+        aggregate_failures do
+          expect(user.name).to_not eq @user_params[:name]
+          expect(user.email).to_not eq @user_params[:email]
+          expect(user.password).to_not eq @user_params[:password]
+          expect(user.password_confirmation).to_not eq @user_params[:password_confirmation]
+        end
+      end
+      it '編集画面に遷移すること' do
+        expect(response.body).to include('ユーザー編集')
+      end
+    end
+
+    context '有効な値の場合' do
+      before do
+        @name  = "Foo Bar"
+        @email = "foo@bar.com"
+        patch user_path(user), params: { user: { name:  @name,
+                                                    email: @email,
+                                                    password:              "",
+                                                    password_confirmation: "" } }
+      end
+
+      it '更新されること' do
+        user.reload
+        aggregate_failures do
+          expect(user.name).to eq @name
+          expect(user.email).to eq @email
+        end
+      end
+
+      it 'ユーザー情報画面へリダイレクトすること' do
+        expect(response).to redirect_to user
+      end
+
+      it 'flashが表示されていること' do
+        expect(flash).to be_any
+      end
+    end
+  end
 end
