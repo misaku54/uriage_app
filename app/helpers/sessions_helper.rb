0809c9941591eaa46b53d09ed7ea60f25b05afc9
misaku54 <eisei0504@outlook.jp>
2023-03-18 13:53:52 +0900

    Update app/helpers/sessions_helper.rb current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,8 +13,15 @@ module SessionsHelper
 
   # 現在のユーザーでログインする
   def current_user
-    if session[:user_id]
+    # セッションがある場合、セッションでカレントユーザーを取得。クッキーがある場合、クッキーでカレントユーザーを取得。
+    if (user_id =  session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user&.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
