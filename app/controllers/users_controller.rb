442a20a63a577b972f6e34a95743f763a6c56795
misaku54 <eisei0504@outlook.jp>
2023-03-20 16:08:06 +0900

    Add session_token mthod

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,6 +54,6 @@ class UsersController < ApplicationController
     # 遷移したページのidがログインユーザー本人でなければ、ホーム画面へリダイレクト
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url, status: :see_other) unless @user == current_user
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,8 @@ module SessionsHelper
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+    # セッションリプレイ攻撃から保護する
+    session[:session_token] = user.session_token
   end
 
   # 永続セッションのためにユーザーをデータベースに記憶する
@@ -13,9 +15,12 @@ module SessionsHelper
 
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    # セッションがある場合、セッションでカレントユーザーを取得。クッキーがある場合、クッキーでカレントユーザーを取得。
+    # 記憶トークンのcookieに対応するユーザーを返す
     if (user_id =  session[:user_id])
-      @current_user ||= User.find_by(id: session[:user_id])
+      user = User.find_by(id: session[:user_id])
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user&.authenticated?(cookies[:remember_token])
@@ -23,6 +28,11 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
   end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
