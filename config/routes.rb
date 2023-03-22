5c315526d096e191f736017280d45bd4f227f1e5
misaku54 <eisei0504@outlook.jp>
2023-03-22 21:18:31 +0900

    update pagenate center & fix strtime

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  resources :users, only: [:index, :show] 
   namespace :admin do
     resources :users
     get    '/signup',  to: 'users#new'
