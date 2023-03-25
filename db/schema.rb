83094d597e937d5e409c5b2cbb6cb8be4dbe0323
misaku54 <eisei0504@outlook.jp>
2023-03-25 09:38:00 +0900

    Add model maker db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_083550) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_25_003242) do
+  create_table "makers", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_makers_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -22,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_083550) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "makers", "users"
 end
