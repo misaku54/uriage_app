3273d96069e44d6c86f86a5dec739369a9075f04
misaku54 <eisei0504@outlook.jp>
2023-04-02 18:54:16 +0900

    add new_action sales_controller

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -3,8 +3,19 @@ class SalesController < ApplicationController
   end
 
   def new
+    @user   = User.find(params[:user_id])
+    @makers = @user.makers
+    @producttypes = @user.producttypes
+    @sale   = @user.sales.build
   end
 
   def edit
+  end
+
+
+  private
+  # ストロングパラメータ
+  def sale_params
+    params.require(:sale).permit(:amount_sold, :remark, :maker_id, :producttype_id)
   end
 end
