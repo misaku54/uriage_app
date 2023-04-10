fcd1534b89b55a2db4ed03b7d44c4e58e52e7a99
misaku54 <eisei0504@outlook.jp>
2023-04-11 06:37:21 +0900

    bundle install chartkick

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.4/dist/jquery.js"
 pin_all_from "app/javascript/custom",      under: "custom"
+pin "chartkick", to: "chartkick.js"
+pin "Chart.bundle", to: "Chart.bundle.js"
