13b9b580c30199673bde02fe1013232808660349
misaku54 <eisei0504@outlook.jp>
2023-04-11 17:30:13 +0900

    aggregate_result.html.erbパーシャルテンプレート化後

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -1,10 +1,16 @@
 module SalesHelper
   # インスタンスをchartkickメソッドの引数に渡す用の配列に変換する。
-  def convert_array(aggregates)
+  def convert_array(aggregates, card_pattern)
     if aggregates.present?
-      aggregates.map do |aggregate|
-        ["#{aggregate.maker_name} #{aggregate.producttype_name}",
-        aggregate.sum_amount_sold] 
+      if card_pattern == "maker_producttype"
+        aggregates.map do |aggregate|
+          ["#{aggregate.maker_name} #{aggregate.producttype_name}",
+          aggregate.sum_amount_sold] 
+        end
+      else
+        aggregates.map do |aggregate|
+          [aggregate.name, aggregate.sum_amount_sold] 
+        end
       end
     else
       return nil
