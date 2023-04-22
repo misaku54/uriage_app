7a6a24bc5ee21b7f832a8db21888292e90097a29
misaku54 <eisei0504@outlook.jp>
2023-04-22 12:56:05 +0900

    add_enヘルパーの削除。売上モデルのリファクタリング

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,26 +1,28 @@
 class Sale < ApplicationRecord
   include SessionsHelper
+  # 関連付け
   belongs_to :user
   belongs_to :maker
   belongs_to :producttype
 
-  # 販売価格は数値のみで1円以上
-  validates :amount_sold, numericality: { greater_than_or_equal_to: 1 }
-  # 備考は1000文字まで
+  # バリデーション
+  validates :amount_sold, numericality: { greater_than_or_equal_to: 1 } #売上登録できる販売額は１円以上でなければならない。
   validates :remark, length: { maximum: 1000 }
   validates :created_at, presence: true
-  # 登録していないメーカー名や商品名で更新できない
-  validate :maker_id_should_be_registered
-  validate :producttype_id_should_be_registered
+  # カスタムバリデーション
+  validate :maker_id_should_be_registered       #売上登録できるメーカー名は、メーカーマスタに登録されているものでなければならない
+  validate :producttype_id_should_be_registered #売上登録できる商品分類名は、商品分類マスタに登録されているものでなければならない
 
-  # 集計用SQL
+  # scopeで使う集計用SQL
   sql_1 = <<-EOS
   SELECT k.maker_name, k.producttype_name, k.sum_amount_sold current_year_amount, k.quantity_sold current_year_quantity,
   COALESCE(z.sum_amount_sold, 0) last_year_amount, COALESCE(z.quantity_sold, 0) last_year_quantity
     FROM (SELECT m.name maker_name, p.name producttype_name, SUM(s.amount_sold) sum_amount_sold, COUNT(*) quantity_sold
       FROM sales s
-      INNER JOIN makers m ON m.id = s.maker_id
-      INNER JOIN producttypes p ON p.id = s.producttype_id
+      INNER JOIN makers m 
+      ON m.id = s.maker_id
+      INNER JOIN producttypes p 
+      ON p.id = s.producttype_id
       WHERE s.user_id = :user_id AND s.created_at BETWEEN :current_year_start AND :current_year_end
       GROUP BY maker_name, producttype_name) k
     LEFT JOIN (SELECT m.name maker_name, p.name producttype_name, SUM(s.amount_sold) sum_amount_sold, COUNT(*) quantity_sold
@@ -73,6 +75,7 @@ class Sale < ApplicationRecord
     ORDER BY current_year_amount DESC
   EOS
 
+  # スコープ
   # メーカー、商品別の合計販売額と販売数を集計する。
   scope :maker_producttype_sum_amount_sold, -> (user, search_params) { find_by_sql([sql_1,{ user_id: user.id,
                                                                                             current_year_start: search_params.date.in_time_zone.beginning_of_year,
@@ -93,12 +96,12 @@ class Sale < ApplicationRecord
                                                                                       last_year_end: search_params.date.in_time_zone.prev_year.end_of_year }]) }
   # 販売額の合計を降順で並び替え
   scope :sorted, -> { order('sum_amount_sold DESC') }
-    
+  
+
   private 
 
-  # バリデーションメソッド
   def maker_id_should_be_registered
-    # 空白やnilの場合はこのバリデーションをスキップする。
+    # カスタムバリデーションでallow_nilがやりたかった。
     unless maker_id.blank?
       select_maker = Maker.find_by(id: maker_id, user_id: user_id)
       errors.add(:maker_id, 'は不正な値です') unless select_maker
