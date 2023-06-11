50e9eb9f68a86b69e2ef3eda50ef77b6f54888fb
misaku54 <eisei0504@outlook.jp>
2023-06-12 00:39:32 +0900

    [add]ユーザー管理のシステムスペックを作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -16,50 +16,33 @@ RSpec.describe "ユーザー管理機能（一般）", type: :system do
     end
   end
 
-  # describe 'ログイン中' do
-  #   before do
-  #     visit login_path
-  #     fill_in 'メールアドレス', with: login_user.email
-  #     fill_in 'パスワード', with: login_user.password
-  #     click_button 'ログイン'
-  #   end
+  describe 'ログイン中' do
+    before do
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログイン'
+    end
 
-  #   describe '詳細表示機能' do
-  #     context 'ユーザーAがログインしている場合' do
-  #       let(:login_user) { user_a }
+    describe '詳細表示機能' do
+      context 'ユーザーAでログインしている場合' do
+        let(:login_user) { user_a }
         
-  #       it '' do
+        it 'ユーザーAの詳細画面が表示されていること' do
+          expect(page).to have_current_path user_path(user_a)
+          expect(page).to have_content 'ユーザーA'
+          expect(page).to have_content 'a@example.com'
+        end
+      end
 
-  #       end
-  #     end
-  #   end
-  # end
-  # describe '新規登録機能' do
-  #   context '無効な値で新規登録した場合' do
-  #     it 'エラーメッセージが表示されていること' do
-  #       visit signup_path
-  #       fill_in '名前', with: ''
-  #       fill_in 'メールアドレス', with: 'user@invlid'
-  #       fill_in 'パスワード', with: 'foo'
-  #       fill_in 'パスワード確認', with: 'bar'
-  #       click_button 'ユーザー登録'
-  #       aggregate_failures do
-  #         expect(page).to have_selector 'div#error_explanation'
-  #         expect(page).to have_selector 'div.field_with_errors'
-  #       end
-  #     end
-  #   end
-
-  #   context '有効な値で新規登録した場合' do
-  #     it '成功用のフラッシュメッセージが表示されていること' do
-  #       visit signup_path
-  #       fill_in '名前', with: 'Example User'
-  #       fill_in 'メールアドレス', with: 'user@example.com'
-  #       fill_in 'パスワード', with: 'password'
-  #       fill_in 'パスワード確認', with: 'password'
-  #       click_button 'ユーザー登録'
-  #       expect(page).to have_selector 'div.alert.alert-success'
-  #     end
-  #   end
-  # end
+      context 'ユーザーBでログインしている場合' do
+        let(:login_user) { user_b }
+      
+        it 'ユーザーAの詳細画面に遷移するとホーム画面へ遷移すること' do
+          visit user_path(user_a)
+          expect(page).to have_current_path root_path
+        end
+      end
+    end
+  end
 end
