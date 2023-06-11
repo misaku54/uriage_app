3ac412e5be1e474a54189c26d3da1671ec63687e
misaku54 <eisei0504@outlook.jp>
2023-06-12 03:14:57 +0900

    [update]一覧表示機能のスペックを追加

--- spec/system/admin/users_spec.rb
+++ spec/system/admin/users_spec.rb
@@ -2,12 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "ユーザー管理機能（管理者）", type: :system do
   let(:admin_user) { FactoryBot.create(:admin_user) }
-  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
-  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  let!(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let!(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
   
   describe '未ログイン' do
     describe 'ページ遷移確認' do
-      context '詳細画面へアクセス' do
+      context 'ユーザー一覧画面へアクセス' do
+        it 'ログイン画面へ遷移し、フラッシュが表示されること' do
+          visit admin_users_path
+          expect(page).to have_current_path login_path
+          expect(page).to have_selector 'div.alert.alert-danger'
+        end
+      end
+
+      context 'ユーザー詳細画面へアクセス' do
         it 'ログイン画面へ遷移し、フラッシュが表示されること' do
           visit admin_user_path(admin_user)
           expect(page).to have_current_path login_path
@@ -15,11 +23,40 @@ RSpec.describe "ユーザー管理機能（管理者）", type: :system do
         end
       end
 
-      context '新規ユーザー登録画面へアクセス' do
+      context 'ユーザー登録画面へアクセス' do
         it 'ログイン画面へ遷移し、フラッシュが表示されること' do
           visit new_admin_user_path
           expect(page).to have_current_path login_path
           expect(page).to have_selector 'div.alert.alert-danger'
+        end
+      end
+
+      context 'ユーザー編集画面へアクセス' do
+        it 'ログイン画面へ遷移し、フラッシュが表示されること' do
+          visit edit_admin_user_path(:user_a)
+          expect(page).to have_current_path login_path
+          expect(page).to have_selector 'div.alert.alert-danger'
+        end 
+      end
+    end
+  end
+
+  describe 'ログイン中' do
+    before do
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログイン'
+    end
+
+    describe '一覧表示機能' do
+      context '管理者でログインしている場合' do
+        let(:login_user) { admin_user }
+
+        it 'ユーザーAの情報が表示されていること' do
+          visit admin_users_path
+          expect(page).to have_content 'ユーザーA'
+          expect(page).to have_content 'a@example.com'
         end
       end
     end
