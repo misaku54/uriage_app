fd65a42012fe031244dae46a4c41477980040092
misaku54 <eisei0504@outlook.jp>
2023-06-12 16:53:48 +0900

    [update]ログイン時のページ遷移確認のテストケースを追加

--- spec/system/admin/users_spec.rb
+++ spec/system/admin/users_spec.rb
@@ -49,8 +49,74 @@ RSpec.describe "ユーザー管理機能（管理者）", type: :system do
       click_button 'ログイン'
     end
 
+    describe 'ページ遷移確認' do
+      context '一般ユーザーでログイン時' do
+        let(:login_user) { user_a }
+
+        context 'ユーザー一覧画面へアクセス' do
+          it 'ホーム画面へ遷移すること' do
+            visit admin_users_path
+            expect(page).to have_current_path root_path
+          end
+        end
+
+        context 'ユーザー詳細画面へアクセス' do
+          it 'ホーム画面へ遷移すること' do
+            visit admin_user_path(user_a)
+            expect(page).to have_current_path root_path
+          end
+        end
+
+        context 'ユーザー登録画面へアクセス' do
+          it 'ホーム画面へ遷移すること' do
+            visit new_admin_user_path(user_a)
+            expect(page).to have_current_path root_path
+          end
+        end
+
+        context 'ユーザー編集画面へアクセス' do
+          it 'ホーム画面へ遷移すること' do
+            visit edit_admin_user_path(user_a)
+            expect(page).to have_current_path root_path
+          end
+        end
+      end
+
+      context '管理者でログイン時' do
+        let(:login_user) { admin_user }
+
+        context 'ユーザー一覧画面へアクセス' do
+          it '正常に遷移すること' do
+            visit admin_users_path
+            expect(page).to have_current_path admin_users_path
+          end
+        end
+
+        context 'ユーザー詳細画面へアクセス' do
+          it '正常に遷移すること' do
+            visit admin_user_path(admin_user)
+            expect(page).to have_current_path admin_user_path(admin_user)
+          end
+        end
+
+        context 'ユーザー登録画面へアクセス' do
+          it '正常に遷移すること' do
+            visit new_admin_user_path(admin_user)
+            expect(page).to have_current_path new_admin_user_path(admin_user)
+          end
+        end
+
+        context 'ユーザー編集画面へアクセス' do
+          it '正常に遷移すること' do
+            visit edit_admin_user_path(admin_user)
+            expect(page).to have_current_path edit_admin_user_path(admin_user)
+          end
+        end
+      end 
+    end
+
     describe '一覧表示機能' do
-      context '管理者でログインしている場合' do
+      context '管理者でログイン時' do
         let(:login_user) { admin_user }
 
         it 'ユーザーA、ユーザーBの情報が表示されていること' do
@@ -61,19 +127,14 @@ RSpec.describe "ユーザー管理機能（管理者）", type: :system do
           expect(page).to have_content 'b@example.com'
         end
       end
+    end
 
-      context '一般ユーザーでログインしている場合' do
-        let(:login_user) { user_a }
-
-        it 'ホーム画面へリダイレクト遷移すること' do
-          visit admin_users_path
-          expect(page).to have_current_path root_path
-        end
-      end
+    describe 'ユーザー登録機能' do
+      
     end
 
     describe '編集機能' do
-      
+
     end
   end
 end
