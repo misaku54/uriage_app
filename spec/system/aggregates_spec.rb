0e509f3b12d26f646ed5e7977941c7de78c04048
misaku54 <eisei0504@outlook.jp>
2023-06-14 19:19:56 +0900

    [add]画面遷移のスペックを追加

--- spec/system/aggregates_spec.rb
+++ spec/system/aggregates_spec.rb
@@ -3,21 +3,23 @@ require 'rails_helper'
 RSpec.describe "集計機能", type: :system do
 
   describe '未ログイン' do
-    context '年次集計画面へアクセス' do
-      it 'ログイン画面へ遷移し、フラッシュが表示されること' do
-
+    describe 'ページ遷移確認' do
+      context '年次集計画面へアクセス' do
+        it 'ログイン画面へ遷移し、フラッシュが表示されること' do
+          
+        end
       end
-    end
 
-    context '月次集計画面へアクセス' do
-      it 'ログイン画面へ遷移し、フラッシュが表示されること' do
+      context '月次集計画面へアクセス' do
+        it 'ログイン画面へ遷移し、フラッシュが表示されること' do
 
+        end
       end
-    end
 
-    context '日次集計画面へアクセス' do
-      it 'ログイン画面へ遷移し、フラッシュが表示されること' do
-        
+      context '日次集計画面へアクセス' do
+        it 'ログイン画面へ遷移し、フラッシュが表示されること' do
+
+        end
       end
     end
   end
@@ -27,14 +29,54 @@ RSpec.describe "集計機能", type: :system do
       log_in(login_user)
     end
 
-    describe '年次集計機能' do
+    describe 'ページ遷移確認' do
       context 'ユーザーAでログインしている場合' do
+        let(:login_user) { user_a }
 
+        context 'ユーザーAに紐づく年次集計画面へアクセス' 
+          it '正常に遷移すること' do
+            
+          end
+        end
+
+        context 'ユーザーAに紐づく月次集計画面へアクセス' 
+          it '正常に遷移すること' do
+            
+          end
+        end
+        
+        context 'ユーザーAに紐づく日次集計画面へアクセス' 
+          it '正常に遷移すること' do
+            
+          end
+        end
       end
 
       context 'ユーザーBでログインしている場合' do
-        it 'ユーザーAの画面の'
+        let(:login_user) { user_b }
+        
+        context 'ユーザーAに紐づく年次集計画面へアクセス' 
+          it 'ホーム画面へ遷移すること' do
+            
+          end
+        end
+
+        context 'ユーザーAに紐づく月次集計画面へアクセス' 
+          it 'ホーム画面へ遷移すること' do
+            
+          end
+        end
+        
+        context 'ユーザーAに紐づく日次集計画面へアクセス' 
+          it 'ホーム画面へ遷移すること' do
+            
+          end
+        end
       end
+    end
+    
+    describe '年次集計機能' do
+      
     end
     
     describe '月次集計機能' do
