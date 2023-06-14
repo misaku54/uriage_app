a57e38251d2864e2e7a9ce76ccd9a4094a528d56
misaku54 <eisei0504@outlook.jp>
2023-06-14 15:07:19 +0900

    [add]編集削除ボタンの表示表示確認を追加

--- spec/system/makers_spec.rb
+++ spec/system/makers_spec.rb
@@ -42,12 +42,19 @@ RSpec.describe "メーカー管理機能", type: :system do
     end
 
     describe '一覧表示機能' do
+      before do
+        visit user_makers_path(login_user)
+      end
       context 'ユーザーAでログインしている場合' do
         let(:login_user) { user_a }
 
         it 'ユーザーAが作成したメーカーが表示されていること' do
-          visit user_makers_path(login_user)
           expect(page).to have_content 'メーカーA'
+        end
+
+        it 'ユーザーAが作成したメーカーの編集ボタンと削除ボタンが表示されていること' do
+          expect(page).to have_link '編集', href: "/users/#{maker.user.id}/makers/#{maker.id}/edit"
+          expect(page).to have_link '削除', href: "/users/#{maker.user.id}/makers/#{maker.id}"
         end
       end
 
@@ -55,7 +62,6 @@ RSpec.describe "メーカー管理機能", type: :system do
         let(:login_user) { user_b }
 
         it 'ユーザーAが作成したメーカーが表示されていないこと' do
-          visit user_makers_path(login_user)
           expect(page).to_not have_content 'メーカーA'
         end
       end
