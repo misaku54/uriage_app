db7133eb5936de3b270ba783923c1563c8599df5
misaku54 <eisei0504@outlook.jp>
2023-06-16 03:46:56 +0900

    [add]月次集計と年次集計のスペックとファクトリー追加

--- spec/factories/sales.rb
+++ spec/factories/sales.rb
@@ -4,7 +4,28 @@ FactoryBot.define do
     remark { '期間限定商品' }
     created_at { Time.current }
     association :user
-    maker { FactoryBot.create(:maker,user: user) }
+    maker { FactoryBot.create(:maker, user: user) }
     producttype { FactoryBot.create(:producttype,user: user) } 
   end
+
+  # 年次集計用
+  factory :yearly_aggregate_sale, class: Sale do
+    sequence(:amount_sold) { 1000 }
+    sequence(:remark) { |n| "Test yearly_aggregate #{n}" }
+    # 2022年の1月〜12月の売り上げデータを月毎に１件ずつ生成
+    sequence(:created_at) { |n| Time.local(2023, 1, 1).prev_month(n) } 
+    user 
+    maker
+    producttype
+  end 
+
+  # 月次集計用
+  factory :monthly_aggregate_sale, class: Sale do
+    sequence(:amount_sold) { 1000 }
+    sequence(:remark) { |n| "Test daily_aggregate #{n}" }
+    sequence(:created_at) { |n| Time.local(2023, 1, 1).prev_day(n)  } 
+    user 
+    maker
+    producttype
+  end 
 end
--- spec/system/aggregates_spec.rb
+++ spec/system/aggregates_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "集計機能", type: :system do
-  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') } 
-  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') } 
+  let!(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') } 
+  let!(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') } 
+
+  let!(:producttype_a) { FactoryBot.create(:producttype, name:'商品A', user: user_a) }
+  let!(:producttype_b) { FactoryBot.create(:producttype, name:'商品B', user: user_a) }
+  let!(:producttype_c) { FactoryBot.create(:producttype, name:'商品C', user: user_a) }
+  let!(:maker_a) { FactoryBot.create(:maker, name:'メーカーA', user: user_a) }
+  let!(:maker_b) { FactoryBot.create(:maker, name:'メーカーB', user: user_a) }
+  let!(:maker_c) { FactoryBot.create(:maker, name:'メーカーC', user: user_a) }
+
 
   describe '未ログイン' do
     describe 'ページ遷移確認' do
@@ -90,11 +98,84 @@ RSpec.describe "集計機能", type: :system do
     end
     
     describe '年次集計機能' do
+      let(:login_user) { user_a }
+      let!(:yearly_aggregate_sale) {FactoryBot.create_list(:yearly_aggregate_sale, 12, user: user_a, maker: maker_a, producttype: producttype_a)}
 
+      before do
+        visit user_yearly_aggregate_path(login_user)
+      end
+
+      context '2022年で集計ボタンを押下した場合' do
+        it '集計結果が表示されること' do
+          select '2022', from: 'search_form[date(1i)]'
+          click_button '集計'
+          expect(page).to have_content '合計純売上'
+          expect(page).to have_content '売上推移'
+          expect(page).to have_content '売上ランキング（メーカー、商品分類別）'
+          expect(page).to have_content '売上ランキング（メーカー別）'
+          expect(page).to have_content '売上ランキング（商品分類別）'
+          expect(page).to have_content '集計結果（メーカー、商品分類別の販売額の合計）'
+          expect(page).to have_content '集計結果（メーカー別の販売額の合計）'
+          expect(page).to have_content '集計結果（商品分類別の販売額の合計）'
+        end
+      end 
+
+      context 'データのない年度で集計ボタンを押下した場合' do
+        it '集計結果が表示されないこと' do
+          # ２週目の時にでsequencenの値が継続しているため、2021年のデータが作成されてしまう。そのため、テスト対象の年度を2020とした。
+          select '2020', from: 'search_form[date(1i)]'
+          click_button '集計'
+          expect(page).to have_no_content '合計純売上'
+          expect(page).to have_no_content '売上推移'
+          expect(page).to have_no_content '売上ランキング（メーカー、商品分類別）'
+          expect(page).to have_no_content '売上ランキング（メーカー別）'
+          expect(page).to have_no_content '売上ランキング（商品分類別）'
+          expect(page).to have_no_content '集計結果（メーカー、商品分類別の販売額の合計）'
+          expect(page).to have_no_content '集計結果（メーカー別の販売額の合計）'
+          expect(page).to have_no_content '集計結果（商品分類別の販売額の合計）'
+        end
+      end
     end
     
     describe '月次集計機能' do
-      
+      let(:login_user) { user_a }
+      let!(:monthly_aggregate_sale) {FactoryBot.create_list(:monthly_aggregate_sale, 30, user: user_a, maker: maker_a, producttype: producttype_a)}
+
+      before do
+        visit user_monthly_aggregate_path(login_user)
+      end
+
+      context '2022年12月で集計ボタンを押下した場合' do
+        it '集計結果が表示されること' do
+          select '2022', from: 'search_form[date(1i)]'
+          select '12', from: 'search_form[date(2i)]'
+          click_button '集計する'
+          expect(page).to have_content '合計純売上'
+          expect(page).to have_content '売上推移'
+          expect(page).to have_content '売上ランキング（メーカー、商品分類別）'
+          expect(page).to have_content '売上ランキング（メーカー別）'
+          expect(page).to have_content '売上ランキング（商品分類別）'
+          expect(page).to have_content '集計結果（メーカー、商品分類別の販売額の合計）'
+          expect(page).to have_content '集計結果（メーカー別の販売額の合計）'
+          expect(page).to have_content '集計結果（商品分類別の販売額の合計）'
+        end
+      end
+
+      context 'データのない年月で集計ボタンを押下した場合' do
+        it '集計結果が表示されないこと' do
+          select '2022', from: 'search_form[date(1i)]'
+          select '9', from: 'search_form[date(2i)]'
+          click_button '集計する'
+          expect(page).to have_no_content '合計純売上'
+          expect(page).to have_no_content '売上推移'
+          expect(page).to have_no_content '売上ランキング（メーカー、商品分類別）'
+          expect(page).to have_no_content '売上ランキング（メーカー別）'
+          expect(page).to have_no_content '売上ランキング（商品分類別）'
+          expect(page).to have_no_content '集計結果（メーカー、商品分類別の販売額の合計）'
+          expect(page).to have_no_content '集計結果（メーカー別の販売額の合計）'
+          expect(page).to have_no_content '集計結果（商品分類別の販売額の合計）'
+        end
+      end
     end
 
     describe '日次集計機能' do
