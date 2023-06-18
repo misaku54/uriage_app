355fa4ea4d19c614f7114cf0c4c57223148618a5
misaku54 <eisei0504@outlook.jp>
2023-06-18 19:25:40 +0900

    [fix]不要な行の削除

--- app/javascript/custom/accordion.js
+++ app/javascript/custom/accordion.js
@@ -1,21 +1,5 @@
-// const acc_ttl=Array.from(document.querySelectorAll(".accordion__ttl"));
-
-// for(let i=0; i< acc_ttl.length; i++){
-//   let ttl=acc_ttl[i];
-//   let content=ttl.nextElementSibling;
-//   ttl.addEventListener("click",()=>{
-//     ttl.classList.toggle("is-active");
-//     if (content.style.height) {
-//       content.style.height = null;
-//     } else {
-//       content.style.height = content.scrollHeight + 'px';
-//     }
-//   });
-// }
-// 新しい配列を作成
 document.addEventListener("turbo:load", () => {
   const acc_ttls = document.querySelectorAll('.acordion__ttl');
-  console.log('aaaaa')
   for(let i = 0; i < acc_ttls.length; i++){
     let ttl = acc_ttls[i];
     let content = ttl.nextElementSibling;
