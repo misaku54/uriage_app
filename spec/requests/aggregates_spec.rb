2b797e1d27086f79f531f573d25cc3b5b1ab04cd
misaku54 <eisei0504@outlook.jp>
2023-06-20 18:12:12 +0900

    [update]日次集計のリクエストスペック完了

--- spec/requests/aggregates_spec.rb
+++ spec/requests/aggregates_spec.rb
@@ -155,11 +155,11 @@ RSpec.describe 'Aggregates', type: :request do
             end
 
             it 'インスタンス変数の中身の整合性チェック(@sales_trend)' do
-              # 1月1日  - 1月10日までの売上合計額が、30000円になること
+              # 1月1日  - 1月10日までの売上合計額が、30000円になること(意図：1月1日から10日までは日毎に3000円が入る想定のため、それを確認するためのテスト)
               expect(@sales_trend.values[0..9].sum).to eq 30000
-              # 1月11日 - 1月20日までの売上合計額が、20000円になること
+              # 1月11日 - 1月20日までの売上合計額が、20000円になること(意図：1月11日から20日までは日毎に2000円が入る想定のため、それを確認するためのテスト)
               expect(@sales_trend.values[10..19].sum).to eq 20000
-              # 1月21日 - 1月31日までの売上合計額が、10000円になること
+              # 1月21日 - 1月31日までの売上合計額が、10000円になること(意図：1月21日から30日までは日毎に1000円が入る想定のため、それを確認するためのテスト)
               expect(@sales_trend.values[20..30].sum).to eq 10000
             end
 
@@ -321,50 +321,50 @@ RSpec.describe 'Aggregates', type: :request do
             end
 
             it 'インスタンス変数の中身の整合性チェック(@aggregates_of_producttype)' do
-              # aggregates_of_producttype_1st = @aggregates_of_producttype.first 
-              # aggregates_of_producttype_2nd = @aggregates_of_producttype.second 
-              # aggregates_of_producttype_3rd = @aggregates_of_producttype.third 
-              # # １位、２位、３位のインスタンスにセットされている商品分類名が正しい値か
-              # expect(aggregates_of_producttype_1st.producttype_name).to eq '商品C'
-              # expect(aggregates_of_producttype_2nd.producttype_name).to eq '商品A'
-              # expect(aggregates_of_producttype_3rd.producttype_name).to eq '商品B'
-              # # １位、２位、３位のインスタンスにセットされている合計売上額が正しい値か
-              # expect(aggregates_of_producttype_1st.sum_amount_sold).to eq 30000
-              # expect(aggregates_of_producttype_2nd.sum_amount_sold).to eq 20000
-              # expect(aggregates_of_producttype_3rd.sum_amount_sold).to eq 10000
-              # # １位、２位、３位のインスタンスにセットされている合計販売数量が正しい値か
-              # expect(aggregates_of_producttype_1st.quantity_sold).to eq 30
-              # expect(aggregates_of_producttype_2nd.quantity_sold).to eq 20
-              # expect(aggregates_of_producttype_3rd.quantity_sold).to eq 10
-              # # １位、２位、３位のインスタンスにセットされている去年の合計売上額が正しい値か
-              # expect(aggregates_of_producttype_1st.last_year_sum_amount_sold).to eq 15000
-              # expect(aggregates_of_producttype_2nd.last_year_sum_amount_sold).to eq 10000
-              # expect(aggregates_of_producttype_3rd.last_year_sum_amount_sold).to eq 5000
-              # # １位、２位、３位のインスタンスにセットされている去年の合計販売数量が正しい値か
-              # expect(aggregates_of_producttype_1st.last_year_quantity_sold).to eq 30
-              # expect(aggregates_of_producttype_2nd.last_year_quantity_sold).to eq 20
-              # expect(aggregates_of_producttype_3rd.last_year_quantity_sold).to eq 10
-              # # １位、２位、３位のインスタンスにセットされている売上成長率が正しい値か
-              # expect(aggregates_of_producttype_1st.sales_growth_rate).to eq '100.0%'
-              # expect(aggregates_of_producttype_2nd.sales_growth_rate).to eq '100.0%'
-              # expect(aggregates_of_producttype_3rd.sales_growth_rate).to eq '100.0%'
+              aggregates_of_producttype_1st = @aggregates_of_producttype.first 
+              aggregates_of_producttype_2nd = @aggregates_of_producttype.second 
+              aggregates_of_producttype_3rd = @aggregates_of_producttype.third 
+              # １位、２位、３位のインスタンスにセットされている商品分類名が正しい値か
+              expect(aggregates_of_producttype_1st.producttype_name).to eq '商品C'
+              expect(aggregates_of_producttype_2nd.producttype_name).to eq '商品A'
+              expect(aggregates_of_producttype_3rd.producttype_name).to eq '商品B'
+              # １位、２位、３位のインスタンスにセットされている合計売上額が正しい値か
+              expect(aggregates_of_producttype_1st.sum_amount_sold).to eq 15000
+              expect(aggregates_of_producttype_2nd.sum_amount_sold).to eq 10000
+              expect(aggregates_of_producttype_3rd.sum_amount_sold).to eq 5000
+              # １位、２位、３位のインスタンスにセットされている合計販売数量が正しい値か
+              expect(aggregates_of_producttype_1st.quantity_sold).to eq 15
+              expect(aggregates_of_producttype_2nd.quantity_sold).to eq 10
+              expect(aggregates_of_producttype_3rd.quantity_sold).to eq 5
+              # １位、２位、３位のインスタンスにセットされている去年の合計売上額が正しい値か
+              expect(aggregates_of_producttype_1st.last_year_sum_amount_sold).to eq 7500
+              expect(aggregates_of_producttype_2nd.last_year_sum_amount_sold).to eq 5000
+              expect(aggregates_of_producttype_3rd.last_year_sum_amount_sold).to eq 2500
+              # １位、２位、３位のインスタンスにセットされている去年の合計販売数量が正しい値か
+              expect(aggregates_of_producttype_1st.last_year_quantity_sold).to eq 15
+              expect(aggregates_of_producttype_2nd.last_year_quantity_sold).to eq 10
+              expect(aggregates_of_producttype_3rd.last_year_quantity_sold).to eq 5
+              # １位、２位、３位のインスタンスにセットされている売上成長率が正しい値か
+              expect(aggregates_of_producttype_1st.sales_growth_rate).to eq '100.0%'
+              expect(aggregates_of_producttype_2nd.sales_growth_rate).to eq '100.0%'
+              expect(aggregates_of_producttype_3rd.sales_growth_rate).to eq '100.0%'
             end
 
             it 'インスタンス変数の中身の整合性チェック(@sales_trend)' do
-              # # 1月1日  - 1月10日までの売上合計額が、30000円になること
-              # expect(@sales_trend.values[0..9].sum).to eq 30000
-              # # 1月11日 - 1月20日までの売上合計額が、20000円になること
-              # expect(@sales_trend.values[10..19].sum).to eq 20000
-              # # 1月21日 - 1月31日までの売上合計額が、10000円になること
-              # expect(@sales_trend.values[20..30].sum).to eq 10000
+              # 1月1日  - 1月5日までの売上合計額が、15000円になること
+              expect(@sales_trend.values[0..4].sum).to eq 15000
+              # 1月6日 - 1月10日までの売上合計額が、10000円になること
+              expect(@sales_trend.values[5..9].sum).to eq 10000
+              # 1月11日 - 1月15日までの売上合計額が、5000円になること
+              expect(@sales_trend.values[10..14].sum).to eq 5000
             end
 
             it 'インスタンス変数の中身の整合性チェック(@sales_total_amount)' do
-              # expect(@sales_total_amount).to eq 60000
+              expect(@sales_total_amount).to eq 30000
             end
 
             it 'インスタンス変数の中身の整合性チェック(@sales_growth_rate)' do
-              # expect(@sales_growth_rate).to eq '100.0%'
+              expect(@sales_growth_rate).to eq '100.0%'
             end
           end
 
@@ -387,22 +387,22 @@ RSpec.describe 'Aggregates', type: :request do
           end
         end
 
-        # context '失敗した場合' do
-        #   let(:params) { { search_daily: { start_date: '', end_date: '' } } }
+        context '失敗した場合' do
+          let(:params) { { search_daily: { start_date: '', end_date: '' } } }
 
-        #   it 'インスタンス変数に値が入らないこと' do
-        #     expect(@aggregates_of_maker_producttype).to be_nil
-        #     expect(@aggregates_of_maker).to be_nil
-        #     expect(@aggregates_of_producttype).to be_nil
-        #     expect(@sales_trend).to be_nil
-        #     expect(@sales_total_amount).to be_nil
-        #     expect(@sales_growth_rate).to be_nil
-        #   end
+          it 'インスタンス変数に値が入らないこと' do
+            expect(@aggregates_of_maker_producttype).to be_nil
+            expect(@aggregates_of_maker).to be_nil
+            expect(@aggregates_of_producttype).to be_nil
+            expect(@sales_trend).to be_nil
+            expect(@sales_total_amount).to be_nil
+            expect(@sales_growth_rate).to be_nil
+          end
 
-        #   it 'レスポンスが422であること' do
-        #     expect(response).to have_http_status(:unprocessable_entity)
-        #   end
-        # end
+          it 'レスポンスが422であること' do
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+        end
       end
     end
 
