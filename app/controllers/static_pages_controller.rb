5fef991483b770b31f287d0f02fae9decbf014b1
misaku54 <eisei0504@outlook.jp>
2023-07-02 16:27:26 +0900

    [fix]リファクタリング

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      today = Time.zone.now
+      today = Time.zone.today
       sales = current_user.sales.where(created_at: today.all_day)
       if sales.present?
         # 売上推移の取得
@@ -10,6 +10,7 @@ class StaticPagesController < ApplicationController
         @sales_total_amount = sales.sum(:amount_sold)
       end
 
+      # https://api.open-meteo.com/v1/forecast?latitude=31.9167&longitude=131.4167&hourly=temperature_2m,precipitation_probability,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=Asia%2FTokyo&start_date=2023-07-02&end_date=2023-07-02
       # 天気APIとの連携
 
     end
