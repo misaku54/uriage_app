6352c5d191dcec37519e1e713125af9fc146868d
misaku54 <eisei0504@outlook.jp>
2023-07-02 15:23:49 +0900

    [fix]リファクタリング

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      sales = current_user.sales.where(created_at: Time.zone.now.all_day)
+      today = Time.zone.now
+      sales = current_user.sales.where(created_at: today.all_day)
       if sales.present?
         # 売上推移の取得
-        @sales_trend        = sales.group_by_hour(:created_at, range: Time.zone.now.all_day).sum(:amount_sold)
+        @sales_trend        = sales.group_by_hour(:created_at, range: today.all_day).sum(:amount_sold)
         # 売上合計額の取得
         @sales_total_amount = sales.sum(:amount_sold)
       end
+
+      # 天気APIとの連携
+
     end
   end
 end
