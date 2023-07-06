3be894dc067e380f1ad7ce6e8bf824f71d591e16
misaku54 <eisei0504@outlook.jp>
2023-07-06 18:31:13 +0900

    [update]現在の気温を取得できるロジックを実装中

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -29,6 +29,14 @@ class StaticPagesController < ApplicationController
       response = http_client.request(get_request)
       # responseはJSON形式となっているので、JSON.parseでHashに変換する必要がある。
       @data = JSON.parse(response.body)
+
+      # 現在の天気情報を取得するロジック（あとでモデルかヘルパーに回す）
+      
+      # 現在の時間を取得する。
+      hour = Time.zone.now.strftime("%H").to_i
+      puts @data["hourly"]["temperature_2m"]
+      puts "aaaaaaaaaaaaaaaaa#{@data["hourly"]["temperature_2m"][hour]}"
     end
   end
 end
+{"latitude"=>31.9, "longitude"=>131.4375, "generationtime_ms"=>0.5990266799926758, "utc_offset_seconds"=>32400, "timezone"=>"Asia/Tokyo", "timezone_abbreviation"=>"JST", "elevation"=>7.0, "hourly_units"=>{"time"=>"iso8601", "temperature_2m"=>"°C", "precipitation_probability"=>"%", "weathercode"=>"wmo code"}, "hourly"=>{"time"=>["2023-07-06T00:00", "2023-07-06T01:00", "2023-07-06T02:00", "2023-07-06T03:00", "2023-07-06T04:00", "2023-07-06T05:00", "2023-07-06T06:00", "2023-07-06T07:00", "2023-07-06T08:00", "2023-07-06T09:00", "2023-07-06T10:00", "2023-07-06T11:00", "2023-07-06T12:00", "2023-07-06T13:00", "2023-07-06T14:00", "2023-07-06T15:00", "2023-07-06T16:00", "2023-07-06T17:00", "2023-07-06T18:00", "2023-07-06T19:00", "2023-07-06T20:00", "2023-07-06T21:00", "2023-07-06T22:00", "2023-07-06T23:00"], "temperature_2m"=>[26.7, 26.5, 26.2, 26.1, 26.1, 26.0, 26.1, 26.3, 27.3, 28.9, 29.1, 28.7, 28.3, 28.2, 27.7, 27.3, 27.4, 27.5, 28.0, 28.2, 27.6, 27.1, 26.7, 26.5], "precipitation_probability"=>[0, 2, 4, 6, 17, 28, 39, 29, 20, 10, 7, 3, 0, 2, 4, 6, 9, 13, 16, 12, 7, 3, 3, 3], "weathercode"=>[1, 1, 3, 3, 2, 1, 2, 1, 1, 0, 1, 1, 1, 2, 2, 2, 3, 2, 2, 2, 3, 2, 1, 1]}, "daily_units"=>{"time"=>"iso8601", "weathercode"=>"wmo code", "temperature_2m_max"=>"°C", "temperature_2m_min"=>"°C"}, "daily"=>{"time"=>["2023-07-06"], "weathercode"=>[3], "temperature_2m_max"=>[29.1], "temperature_2m_min"=>[26.0]}}
