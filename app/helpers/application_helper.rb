90f82e9faca8ba20cd1412492d1e8cf151af68f6
misaku54 <eisei0504@outlook.jp>
2023-07-06 21:29:56 +0900

    [fix]祝日ではなかった場合の文言の修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,7 +41,7 @@ module ApplicationHelper
 
   # 与えられた日付が祝日だった場合、その祝日名を取得する。
   def get_holiday(date)
-    return '祝日ではありません' unless HolidayJp.holiday?(date) 
+    return '-' unless HolidayJp.holiday?(date) 
     holidays = HolidayJp.between(date, date)
     holidays.first.name
   end
