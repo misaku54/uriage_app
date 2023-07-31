b5caa64a15a2687ceadb7c036b1afd41fab5ec28
misaku54 <eisei0504@outlook.jp>
2023-07-31 19:44:26 +0900

    [fix]アコーディオンが反応しないバグ修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -15,3 +15,4 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import '@fortawesome/fontawesome-free'
 library.add(fas, far, fab)
+// Turbo.session.drive = false
--- app/javascript/custom/accordion.js
+++ app/javascript/custom/accordion.js
@@ -1,5 +1,6 @@
 document.addEventListener("turbo:load", () => {
   const acc_ttls = document.querySelectorAll('.acordion__ttl');
+  console.log('load')
   for(let i = 0; i < acc_ttls.length; i++){
     let ttl = acc_ttls[i];
     let content = ttl.nextElementSibling;
@@ -16,20 +17,21 @@ document.addEventListener("turbo:load", () => {
   }
 });
 
-document.addEventListener("turbo:render", () => {
-  const acc_ttls = document.querySelectorAll('.acordion__ttl');
-  for(let i = 0; i < acc_ttls.length; i++){
-    let ttl = acc_ttls[i];
-    let content = ttl.nextElementSibling;
-    ttl.addEventListener('click', () => {
-      ttl.classList.toggle('is-active');
+// document.addEventListener("turbo:render", () => {
+//   console.log('render')
+//   const acc_ttls = document.querySelectorAll('.acordion__ttl');
+//   for(let i = 0; i < acc_ttls.length; i++){
+//     let ttl = acc_ttls[i];
+//     let content = ttl.nextElementSibling;
+//     ttl.addEventListener('click', () => {
+//       ttl.classList.toggle('is-active');
 
-      // コンテンツの高さがあれば、高さを０に
-      if (content.style.height) {
-        content.style.height = null;
-      } else {
-        content.style.height = content.scrollHeight + 'px';
-      }
-    });
-  }
-});
+//       // コンテンツの高さがあれば、高さを０に
+//       if (content.style.height) {
+//         content.style.height = null;
+//       } else {
+//         content.style.height = content.scrollHeight + 'px';
+//       }
+//     });
+//   }
+// });
