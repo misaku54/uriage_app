cf66ead379045dd4232f24cad21af6022cafa574
misaku54 <eisei0504@outlook.jp>
2023-08-04 19:34:21 +0900

    [fix]リファクタリング

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
     if logged_in?
       # 今日の日付と現在時刻（時間のみ）を取得
       @today = Time.zone.today
-      @hour  = Time.zone.now.strftime("%H").to_i
+      hour   = Time.zone.now.strftime("%H").to_i
 
       sales = current_user.sales.where(created_at: @today.all_day)
       if sales.present?
@@ -17,7 +17,13 @@ class StaticPagesController < ApplicationController
 
       # 現在の天気情報の取得
       api = OpenMeteoService.new
-      @data = api.get_weather_info(@today)
+      if result = api.get_weather_info(@today)
+        @current_temperature  = result[:hourly][:temperature_2m][hour]
+        @current_weather_code = result[:hourly][:weathercode][hour]
+        @max_temperature = result[:daily][:temperature_2m_max][0]
+        @min_temperature = result[:daily][:temperature_2m_min][0]
+        @current_rainfall_probability = result[:hourly][:precipitation_probability][hour]
+      end
     end
   end
 end
