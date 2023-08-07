ee534b5d074ad8274a2cdab8f5bba9d2bb7a5edd
misaku54 <eisei0504@outlook.jp>
2023-08-07 23:01:58 +0900

    [fix]idを主キーにしない文の追加

--- db/migrate/20230807125545_create_weather_forecasts.rb
+++ db/migrate/20230807125545_create_weather_forecasts.rb
@@ -1,7 +1,7 @@
 class CreateWeatherForecasts < ActiveRecord::Migration[7.0]
   def change
-    create_table :weather_forecasts do |t|
-      t.date :aquired_on, null: false, primary_key: true, default: -> { '(CURRENT_DATE)' } 
+    create_table :weather_forecasts, id: false do |t|
+      t.date :aquired_on, null: false, default: -> { '(CURRENT_DATE)' }, primary_key: true
       t.integer :weather_id
       t.float :temp_max
       t.float :temp_min
