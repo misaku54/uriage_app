aa80f30c5f21db86cfe3a3017989b12c291e82d3
misaku54 <eisei0504@outlook.jp>
2023-08-07 22:33:02 +0900

    [update]カラム設定を記載

--- db/migrate/20230807125545_create_weather_forecasts.rb
+++ db/migrate/20230807125545_create_weather_forecasts.rb
@@ -1,7 +1,11 @@
 class CreateWeatherForecasts < ActiveRecord::Migration[7.0]
   def change
     create_table :weather_forecasts do |t|
-
+      t.date :aquired_on, null: false, primary_key: true, default: -> { '(CURRENT_DATE)' } 
+      t.integer :weather_id
+      t.float :temp_max
+      t.float :temp_min
+      t.float :rainfall
       t.timestamps
     end
   end
