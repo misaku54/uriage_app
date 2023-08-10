eb6c8544a28f02f100e0cbd0b0baccebbbbce05e
misaku54 <eisei0504@outlook.jp>
2023-08-11 02:25:14 +0900

    [update]ログイン時のテストを追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  let(:base_title) { 'LiLy' }
+  let!(:user) { FactoryBot.create(:user) }
+
   describe "#home" do
-    before do
-      get root_path
+    context '未ログイン' do
+      before do
+        get root_path
+      end
+
+      it "レスポンスが正常であること" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "タイトルが正常に表示されていること" do
+        expect(response.body).to include('LiLy')
+      end
+
+      it 'ログイン時にしか出ない項目が表示されていないこと' do
+        expect(response.body).to_not include('日付')
+        expect(response.body).to_not include('天気情報')
+        expect(response.body).to_not include('本日の売上')
+      end
     end
-    
-    it "レスポンスが正常であること" do
-      expect(response).to have_http_status(:success)
-    end
-    
-    it "タイトルが正常に表示されていること" do
-      expect(response.body).to include("#{base_title}")
+
+    context 'ログイン中' do
+      before do
+        log_in(user)
+        get root_path
+      end
+
+      it 'レスポンスが正常であること' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'タイトルが正常に表示されていること' do
+        expect(response.body).to include('LiLy')
+      end
+
+      it 'ログイン時にしか出ない項目が表示されていること' do
+        expect(response.body).to include('日付')
+        expect(response.body).to include('天気情報')
+        expect(response.body).to include('本日の売上')
+      end
     end
   end
 end
