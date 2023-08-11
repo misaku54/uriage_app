0f5ccc632efb11d7b3916d88c9db7b3f05c83cf5
misaku54 <eisei0504@outlook.jp>
2023-08-11 20:24:00 +0900

    [fix]リファクタリング

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   
   describe 'バリデーション' do
-    let(:user) { FactoryBot.build(:user) }
+    let!(:user) { FactoryBot.build(:user) }
 
     it 'userが有効であること' do
       expect(user).to be_valid
