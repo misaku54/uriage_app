8166b4eaa8bf6997858e60acbb865b374e3546ad
misaku54 <eisei0504@outlook.jp>
2023-08-12 04:45:46 +0900

    [update]カスタムバリデーションの追加

--- app/models/weather_forecast.rb
+++ app/models/weather_forecast.rb
@@ -1,7 +1,7 @@
 class WeatherForecast < ApplicationRecord
   # :aquired_onのデフォルト値に現在の日付を設定 
   # attribute :aquired_on, default: -> { Time.zone.now }
-  
+
   # 関連付け
   has_many :sales, foreign_key: "created_on"
 
@@ -11,4 +11,11 @@ class WeatherForecast < ApplicationRecord
   validates :temp_max, presence: true, numericality: true
   validates :temp_min, presence: true, numericality: true
   validates :rainfall_sum, presence: true, numericality: true
+  validate :future_day_check
+
+  private 
+
+  def future_day_check
+    errors.add(:aquired_on, 'に未来日を設定できません。') if self.aquired_on > Time.zone.today
+  end
 end
