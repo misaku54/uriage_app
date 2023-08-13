d2a45402edb6229a78a11f94a29dbc2ca1222912
misaku54 <eisei0504@outlook.jp>
2023-08-14 02:03:49 +0900

    [fix]setIntervalのラグを解消

--- app/javascript/static_pages/home.js
+++ app/javascript/static_pages/home.js
@@ -1,7 +1,5 @@
-// dateオブジェクト整形用の変数
+// dateオブジェクト整形用の変数と関数
 const dayOfWeek = ['日', '月', '火', '水', '木', '金', '土'];
-
-// dateオブジェクト整形用の関数
 const zeroPadding = n => ( n < 10 ) ? '0' + n.toString() : n.toString();
 
 const getDateString = date => 
@@ -10,26 +8,26 @@ const getDateString = date =>
 const getTimeString = date =>
   `${zeroPadding(date.getHours())}:${zeroPadding(date.getMinutes())}:${zeroPadding(date.getSeconds())}`;
 
-
-// 画面ロード後の処理
 document.addEventListener('turbo:load', () => {
   const dateDiv = document.getElementById('date');
   const timeDiv = document.getElementById('time');
-  // 初期化
   let newDate = null;
-
-  // 1秒ごとに実行
-  setInterval(() => {
+  const clock = () => {
     // 現在の日付をコンストラクタで取得
-    const now = new Date;
+    const now = new Date();
     // 日にちが変わったら、新しい日付をnewDateにセットし、要素に再出力
     if( newDate !== now.getDate() ) {
         newDate = now.getDate();
         dateDiv.innerText = getDateString(now);
     }
     timeDiv.innerText = getTimeString(now);
-  }, 1000);
+  };
 
-  // 型調査
-  // console.log(typeof date2.getDate())
+  // 要素があった場合に実行
+  if( dateDiv && timeDiv ){
+    clock();
+    // 1秒ごとに実行　ここのコールバックメソッドには()をつけたらダメ！
+    // （）は実行という意味で、つけてしまうとその場でclockメソッドを実行してしまう。
+    setInterval(clock, 1000);
+  };
 });
