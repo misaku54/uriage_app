b47e4b1e6b6cf03bd8a484cee9e78c99d12a701b
misaku54 <eisei0504@outlook.jp>
2023-08-16 07:05:12 +0900

    [update]リアルタイムバリデーションの実装

---
+++ app/javascript/form/validation.js
@@ -0,0 +1,43 @@
+const showErrorMessage = (target, message) => {
+  if(!target.nextElementSibling) {
+    const errorMessage = document.createElement('small');
+    errorMessage.textContent = message;
+    target.insertAdjacentElement("afterend", errorMessage);
+  }
+}
+
+const removeErrorMessage = (target) => {
+  const error = target.nextElementSibling;
+  if(error){
+    error.remove();
+  }
+}
+
+const checkLength = (labelName, maxLength, input) => {
+  if(input.value.length > maxLength) {
+    showErrorMessage(input, `${labelName}は、${maxLength}文字以内にしてください`)
+  }else{
+    removeErrorMessage(input);
+  }
+}
+
+
+
+const inputSelector = document.querySelectorAll('.input');
+inputSelector[0].focus();
+console.log(inputSelector)
+
+for (const input of inputSelector) {
+  input.addEventListener('blur', () => {
+    if(input.value.trim() === ''){
+      showErrorMessage(input, '必須項目です。');
+    }
+  });
+
+  input.addEventListener('input', () => {
+    console.log('aaa')
+    input.name === 'maker[name]' && checkLength('メーカー名', 30, input)
+    
+  
+  });
+}
