876a7731052e5ad6ed651b2205cca97328df4db8
misaku54 <eisei0504@outlook.jp>
2023-08-16 10:08:37 +0900

    カスタムバリデーション試行中

--- app/javascript/form/validation.js
+++ app/javascript/form/validation.js
@@ -1,15 +1,24 @@
-const showErrorMessage = (target, message) => {
+const showErrorMessage = (target, message , parent = null) => {
+  console.log(target.nextElementSibling)
+  const errorMessage = document.createElement('small');
+  errorMessage.textContent = message;
+
   if(!target.nextElementSibling) {
-    const errorMessage = document.createElement('small');
-    errorMessage.textContent = message;
-    target.insertAdjacentElement("afterend", errorMessage);
+    target.insertAdjacentElement('afterend', errorMessage);
+  }
+  if(parent){
+    parent.insertAdjacentElement('beforeend', errorMessage);
   }
 }
 
-const removeErrorMessage = (target) => {
+const removeErrorMessage = (target, parent = null) => {
   const error = target.nextElementSibling;
+  const parent = parent.lastElementChild.classList.contain('samll') || null
   if(error){
     error.remove();
+  }
+  if(parent){
+    parent.insertAdjacentElement('beforeend', errorMessage);
   }
 }
 
@@ -24,19 +33,39 @@ const checkLength = (labelName, maxLength, input) => {
 
 
 const inputSelector = document.querySelectorAll('.input');
-inputSelector[0].focus();
-console.log(inputSelector)
+if(inputSelector) {
+  // inputSelector[0].focus();
+  console.log(inputSelector)
 
-for (const input of inputSelector) {
-  input.addEventListener('blur', () => {
-    if(input.hasAttribute('required') && input.value.trim() === ''){
-      showErrorMessage(input, '必須項目です。');
+  for (const input of inputSelector) {
+    input.addEventListener('blur', () => {
+      if(input.hasAttribute('required') && input.value.trim() === ''){
+        showErrorMessage(input, '必須項目です。');
+      }
+    });
+
+    input.addEventListener('input', () => {
+      input.name === 'maker[name]' && checkLength('メーカー名', 30, input);
+      input.name === 'producttype[name]' && checkLength('商品分類名', 30, input);
+    });
+  }
+}
+
+
+const selectBox = document.querySelectorAll('.selectBox')
+
+console.log(selectBox);
+
+for (const select of selectBox) {
+  const parent = select.parentElement
+  console.log(parent)
+  // select.options[0].selected === true && console.log('irero!')
+  select.addEventListener('change', () => {
+    if(select.options[0].selected === true){
+      console.log('saaaa')
+      showErrorMessage(select, '必須項目です。', parent)
+    }else{
+      removeErrorMessage(select, '必須項目です。', parent)
     }
-  });
-
-  input.addEventListener('input', () => {
-    console.log('aaa')
-    input.name === 'maker[name]' && checkLength('メーカー名', 30, input);
-    input.name === 'producttype[name]' && checkLength('商品分類名', 30, input);
-  });
+  })
 }
