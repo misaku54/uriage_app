5322fb6e20c6aead8558ea2c2cccfca2b02c6f1b
misaku54 <eisei0504@outlook.jp>
2023-08-24 07:38:25 +0900

    [update]試作中　タブ実装　コーディング完了

--- app/javascript/aggregates/tabMenu.js
+++ app/javascript/aggregates/tabMenu.js
@@ -1,18 +1,29 @@
 document.addEventListener('turbo:load',() => {
-  
-  const tabs = document.querySelectorAll('#tab');
+  const removeClass = (tab, item) => {
+    const currentTab = tab.parentNode.querySelector('.current');
+    const activeItem = item.parentNode.querySelector('.active');
+    currentTab.classList.remove('.current');
+    activeItem.classList.remove('.active');
+  };
+
+  const addClass = (tab, item) => {
+    tab.classList.add('.current');
+    item.classList.add('.active');
+  };
+
   const switchElementClass = (e) => {
-    const targetEl = e.target.getAttribute('href')
-  }
+    const targetTab  = e.target;
+    const targetItem = document.querySelector(e.target.getAttribute('href'));
+    // 引数の親要素からcurrent、activeクラスを検索し削除する。
+    removeClass(targetTab, targetItem);
+    // 引数の要素にcurrent、activeクラスを付与する。
+    addClass(targetTab, targetItem);
+  };
 
+  const tabs = document.querySelectorAll('#tab');
   // 要素がなければ終了
-  if(!tabs.length){ return 0 }
-
-
+  if(!tabs.length){ return 0 };
   tabs.forEach((tab) => {
-    tab.addEventListener('click', switchElementClass)
-  })
-  
-  
-  console.log('終了')
-})
+    tab.addEventListener('click', switchElementClass);
+  });
+});
