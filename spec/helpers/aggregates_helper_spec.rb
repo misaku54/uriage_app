5e64e2a0060a795c1fe3570e7cdace150e895042
misaku54 <eisei0504@outlook.jp>
2023-08-29 16:32:07 +0900

    [fix]文言変更

--- spec/helpers/aggregates_helper_spec.rb
+++ spec/helpers/aggregates_helper_spec.rb
@@ -20,100 +20,88 @@ RSpec.describe AggregatesHelper, type: :helper do
       @aggregates_of_producttype       = aggregate.aggregates_of_producttype
     end
 
-    context '引数が有効な値の場合' do
-      context '第二引数がmaker_producttypeの場合' do
-        let(:ary) { [["テスト会社 カバン", 10000]] }
-        it '配列が作成されること' do
-          expect(convert_array(@aggregates_of_maker_producttype, 'maker_producttype')).to eq ary
-        end
-      end
-
-      context '第二引数がmakerの場合' do
-        let(:ary) { [["テスト会社", 10000]] }
-        it '配列が作成されること' do
-          expect(convert_array(@aggregates_of_maker, 'maker')).to eq ary
-        end
-      end
-
-      context '第二引数がproducttypeの場合' do
-        let(:ary) { [["カバン", 10000]] }
-        it '配列が作成されること' do
-          expect(convert_array(@aggregates_of_producttype, 'producttype')).to eq ary
-        end
+    context '第二引数がmaker_producttypeの場合' do
+      let(:ary) { [["テスト会社 カバン", 10000]] }
+      it '配列が作成されること' do
+        expect(convert_array(@aggregates_of_maker_producttype, 'maker_producttype')).to eq ary
       end
     end
 
-    context '引数が無効な値の場合' do
-      context '第二引数が条件外の値の場合' do
-        it 'raiseすること' do
-          expect{convert_array(@aggregates_of_producttype, 'foo')}.to raise_error(ArgumentError, '無効な引数が渡されました。pattern: foo')
-        end
+    context '第二引数がmakerの場合' do
+      let(:ary) { [["テスト会社", 10000]] }
+      it '配列が作成されること' do
+        expect(convert_array(@aggregates_of_maker, 'maker')).to eq ary
+      end
+    end
+
+    context '第二引数がproducttypeの場合' do
+      let(:ary) { [["カバン", 10000]] }
+      it '配列が作成されること' do
+        expect(convert_array(@aggregates_of_producttype, 'producttype')).to eq ary
+      end
+    end
+
+    context '第二引数が条件外の値の場合' do
+      it 'raiseすること' do
+        expect{convert_array(@aggregates_of_producttype, 'foo')}.to raise_error(ArgumentError, '無効な引数が渡されました。pattern: foo')
       end
     end
   end
 
   describe 'make_title' do
-    context '引数が有効な値の場合' do
-      context 'maker_producttypeの場合' do
-        let(:title) { 'メーカー×商品分類' }
-        it '正しい文字列が返ること' do
-          expect(make_title('maker_producttype')).to eq title
-        end
+    context '引数がmaker_producttypeの場合' do
+      let(:title) { 'メーカー×商品分類' }
+      it '正しい文字列が返ること' do
+        expect(make_title('maker_producttype')).to eq title
       end
-      
-      context 'makerの場合' do
-        let(:title) { 'メーカー' }
-        it '正しい文字列が返ること' do
-          expect(make_title('maker')).to eq title
-        end
+    end
+    
+    context '引数がmakerの場合' do
+      let(:title) { 'メーカー' }
+      it '正しい文字列が返ること' do
+        expect(make_title('maker')).to eq title
       end
-      
-      context 'producttypeの場合' do
-        let(:title) { '商品分類' }
-        it '正しい文字列が返ること' do
-          expect(make_title('producttype')).to eq title
-        end
+    end
+    
+    context '引数がproducttypeの場合' do
+      let(:title) { '商品分類' }
+      it '正しい文字列が返ること' do
+        expect(make_title('producttype')).to eq title
       end
     end
 
-    context '引数が無効な値の場合' do
-      context '条件外の値の場合' do
-        it 'raiseすること' do
-          expect{ make_title('foo') }.to raise_error(ArgumentError, '無効な引数が渡されました。pattern: foo')
-        end
+    context '引数が条件外の値の場合' do
+      it 'raiseすること' do
+        expect{ make_title('foo') }.to raise_error(ArgumentError, '無効な引数が渡されました。pattern: foo')
       end
     end
   end
 
   describe 'make_color' do
-    context '引数が有効な値の場合' do
-      context 'maker_producttypeの場合' do
-        let(:color) { '#003793' }
-        it '正しい文字列が返ること' do
-          expect(make_color('maker_producttype')).to eq color
-        end
+    context '引数がmaker_producttypeの場合' do
+      let(:color) { '#003793' }
+      it '正しい文字列が返ること' do
+        expect(make_color('maker_producttype')).to eq color
       end
-      
-      context 'makerの場合' do
-        let(:color) { '#69AADE' }
-        it '正しい文字列が返ること' do
-          expect(make_color('maker')).to eq color
-        end
+    end
+    
+    context '引数がmakerの場合' do
+      let(:color) { '#69AADE' }
+      it '正しい文字列が返ること' do
+        expect(make_color('maker')).to eq color
       end
-      
-      context 'producttypeの場合' do
-        let(:color) { '#009E96' }
-        it '正しい文字列が返ること' do
-          expect(make_color('producttype')).to eq color
-        end
+    end
+    
+    context '引数がproducttypeの場合' do
+      let(:color) { '#009E96' }
+      it '正しい文字列が返ること' do
+        expect(make_color('producttype')).to eq color
       end
     end
 
-    context '引数が無効な値の場合' do
-      context '条件外の値の場合' do
-        it 'raiseすること' do
-          expect{ make_color('foo') }.to raise_error(ArgumentError, '無効な引数が渡されました。pattern: foo')
-        end
+    context '引数が条件外の値の場合' do
+      it 'raiseすること' do
+        expect{ make_color('foo') }.to raise_error(ArgumentError, '無効な引数が渡されました。pattern: foo')
       end
     end
   end
@@ -127,31 +115,30 @@ RSpec.describe AggregatesHelper, type: :helper do
       @aggregates_of_producttype       = aggregate.aggregates_of_producttype
     end
 
-    context '引数が有効な値の場合' do
-      context 'メーカー×商品分類のオブジェクトの場合' do
-        let(:name) { 'テスト会社のカバン' }
-        it '正しい文字列が返ること' do
-          expect(best_selling_name(@aggregates_of_maker_producttype.first)).to eq name
-        end
-      end
-      context 'メーカーのオブジェクトの場合' do
-        let(:name) { 'テスト会社' }
-        it '正しい文字列が返ること' do
-          expect(best_selling_name(@aggregates_of_maker.first)).to eq name
-        end
-      end
-      context '商品分類のオブジェクトの場合' do
-        let(:name) { 'カバン' }
-        it '正しい文字列が返ること' do
-          expect(best_selling_name(@aggregates_of_producttype.first)).to eq name
-        end
+    context '引数がメーカー×商品分類のオブジェクトの場合' do
+      let(:name) { 'テスト会社のカバン' }
+      it '正しい文字列が返ること' do
+        expect(best_selling_name(@aggregates_of_maker_producttype.first)).to eq name
       end
     end
-    context '引数が無効な値の場合' do
-      context 'nilの場合' do
-        it 'raiseすること' do
-          expect{best_selling_name(nil)}.to raise_error(ArgumentError,"無効な引数が渡されました。aggregate:")
-        end
+
+    context '引数がメーカーのオブジェクトの場合' do
+      let(:name) { 'テスト会社' }
+      it '正しい文字列が返ること' do
+        expect(best_selling_name(@aggregates_of_maker.first)).to eq name
+      end
+    end
+    
+    context '引数が商品分類のオブジェクトの場合' do
+      let(:name) { 'カバン' }
+      it '正しい文字列が返ること' do
+        expect(best_selling_name(@aggregates_of_producttype.first)).to eq name
+      end
+    end
+    
+    context '引数が条件外のオブジェクトの場合' do
+      it 'raiseすること' do
+        expect{best_selling_name("foo")}.to raise_error(ArgumentError,"無効な引数が渡されました。aggregate: foo")
       end
     end
   end
