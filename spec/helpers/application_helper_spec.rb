7f62f46fa17f546cd31247c575591562d0215720
misaku54 <eisei0504@outlook.jp>
2023-08-29 13:22:14 +0900

    [update]スペック追加

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -17,8 +17,24 @@ RSpec.describe ApplicationHelper, type: :helper do
 
   describe 'add_comma_en' do
     context '引数を渡した場合' do
-      it '渡した値のカンマ区切りの文字列が返ること' do
+      let(:comma_en) { '10,000円' }
+      it '渡した値のカンマ区切り円の文字列が返ること' do
+        add_comma_en()
+      end
+    end
 
+    context '引数を渡さなかった場合' do
+      it '文字列0が返ってくること' do
+        
+      end
+    end
+  end
+
+  describe 'add_comma' do
+    context '引数を渡した場合' do
+      let(:comma_en) { '10,000' }
+      it '渡した値のカンマ区切りの文字列が返ること' do
+        add_comma_en()
       end
     end
 
@@ -82,33 +98,75 @@ RSpec.describe ApplicationHelper, type: :helper do
         
       end
     end
+
     context '引数が0の場合' do
       it '快晴が返ること' do
 
       end
     end
-    context '引数が0の場合' do
-      it '快晴が返ること' do
+    
+    context '引数が1の場合' do
+      it '晴れが返ること' do
 
       end
     end
-    context '引数が0の場合' do
-      it '快晴が返ること' do
+
+    context '引数が2の場合' do
+      it '一部曇が返ること' do
 
       end
     end
-    context '引数が0の場合' do
-      it '快晴が返ること' do
+    
+    context '引数が3の場合' do
+      it '曇りが返ること' do
 
       end
     end
-    context '引数が0の場合' do
-      it '快晴が返ること' do
+    
+    context '引数が4以上49以下の場合' do
+      it '霧が返ること' do
 
       end
     end
-    context '引数が0の場合' do
-      it '快晴が返ること' do
+    
+    context '引数が50以上59以下の場合' do
+      it '霧雨が返ること' do
+
+      end
+    end
+    
+    context '引数が60以上69以下の場合' do
+      it '雨が返ること' do
+
+      end
+    end
+    
+    context '引数が70以上79以下の場合' do
+      it '雪が返ること' do
+
+      end
+    end
+    
+    context '引数が80以上84以下の場合' do
+      it '俄か雨が返ること' do
+
+      end
+    end
+    
+    context '引数が85以上94以下の場合' do
+      it '雪・雹が返ること' do
+
+      end
+    end
+    
+    context '引数が95以上99以下の場合' do
+      it '雷雨が返ること' do
+
+      end
+    end
+    
+    context 'いずれにも当てはまらない時' do
+      it '不明が返ること' do
 
       end
     end
