b49ee8520ab06e6cba62e324a141e8f30a27ab8d
misaku54 <eisei0504@outlook.jp>
2023-08-30 16:57:55 +0900

    [update]コントローラー記載中

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,32 @@
 class EventsController < ApplicationController
+  before_action :logged_in_user
+  before_action :correct_user
+
   def new
+    @event = @user.events.build
   end
 
   def show
+    @event = @user.events.find(params[:id])
+  end
+
+  def create
+    @event = @user.events.build(event_params)
+    if @event.save
+      flash[:success] = '登録しました。'
+      redirect_to root_url, status: :see_other
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def edit
+    @event = @user.events.find(params[:id])
+    if @event.update(event_params)
+      flash[:success] == '更新しました。'
+      redirect_to root_url, status: :see_other
+    else
+      
+    end
   end
 end
