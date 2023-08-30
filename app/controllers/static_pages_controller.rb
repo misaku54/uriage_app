819135803814973f7d62ea0a467ab1b20827a725
misaku54 <eisei0504@outlook.jp>
2023-08-30 22:51:30 +0900

    [fix]コントローラーの修正[add]フォーム追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,9 +3,13 @@ require 'net/http'
 class StaticPagesController < ApplicationController
   def home
     return unless logged_in?
-    user = current_user
-    # 今日の日付と現在時刻（時間のみ）を取得
+    
     today = Time.zone.now
+    user = current_user
+    @events = user.events
+    # @events = Event.where(start_time: Time.now.beginning_of_month.beginning_of_week..
+    # Time.now.end_of_month.end_of_week)
+    # 今日の日付と現在時刻（時間のみ）を取得
     sales = user.sales.where(created_at: today.all_day)
     return if sales.blank?
 
@@ -13,8 +17,5 @@ class StaticPagesController < ApplicationController
     @sales_trend = sales.group_by_hour(:created_at, range: today.all_day).sum(:amount_sold)
     # 売上合計額の取得
     @sales_total_amount = sales.sum(:amount_sold)
-    # @events = user.events
-    @events = Event.where(start_time: Time.now.beginning_of_month.beginning_of_week..
-    Time.now.end_of_month.end_of_week)
   end
 end
