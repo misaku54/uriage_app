9631db3a9ef500ac32c386787b6ce75a0f58249b
misaku54 <eisei0504@outlook.jp>
2023-08-31 23:00:47 +0900

    [update]バリデーション追加[fix]リファクタリング

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,16 +3,13 @@ require 'net/http'
 class StaticPagesController < ApplicationController
   def home
     return unless logged_in?
-    
     today = Time.zone.now
     @user = current_user
     @events = @user.events
-    # @events = Event.where(start_time: Time.now.beginning_of_month.beginning_of_week..
-    # Time.now.end_of_month.end_of_week)
+
     # 今日の日付と現在時刻（時間のみ）を取得
     sales = @user.sales.where(created_at: today.all_day)
     return if sales.blank?
-
     # 売上推移の取得
     @sales_trend = sales.group_by_hour(:created_at, range: today.all_day).sum(:amount_sold)
     # 売上合計額の取得
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,21 @@
 class Event < ApplicationRecord
   # 関連付け
   belongs_to :user
+
+  # バリデーション
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :content, length: { maximum: 1000 }
+  validates :start_time, presence: true 
+  validates :end_time, presence: true 
+
+  # カスタムバリデーション
+  validate :check_time
+
+  private
+
+  def check_time
+    if start_time.present? && end_time.present?
+      errors.add(:end_time, 'は開始時刻より前の日を設定することはできません。') if end_time < start_time
+    end
+  end
 end
