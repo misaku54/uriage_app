cc1f45382004f3dcc5880e495fb5582d10f73952
misaku54 <eisei0504@outlook.jp>
2023-09-01 01:58:38 +0900

    [fix]インデント修正

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,10 +2,10 @@ module StaticPagesHelper
   def create_event_tag(obj)
     start_date = l(obj.start_time, format: :cal_title)
     end_date   = l(obj.end_time, format: :cal_title)
-    start_date_time = l(obj.start_time, format: :short)
-    end_date_time   = l(obj.end_time, format: :short)
     start_time = l(obj.start_time, format: :cal_short)
     end_time   = l(obj.end_time, format: :cal_short)
+    start_date_time = l(obj.start_time, format: :short)
+    end_date_time   = l(obj.end_time, format: :short)
 
     return tag.div("#{start_date_time} - #{end_date_time}") unless start_date == end_date
     tag.div("#{start_time} - #{end_time}")
