8de32d7973ed66c0a8932fb78319eee2407feeee
misaku54 <eisei0504@outlook.jp>
2023-09-01 15:34:02 +0900

    [update]新規登録のテスト完了

--- spec/system/event_spec.rb
+++ spec/system/event_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "スケジュール管理機能", type: :system do
+RSpec.describe "カレンダー管理機能", type: :system do
   let!(:event) { FactoryBot.create(:event) }
   let(:user_a) { event.user }
   let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
 
   describe '未ログイン' do
     describe 'ページ遷移確認' do
-      context 'スケジュールの新規登録画面へアクセス' do
+      context 'カレンダーの新規登録画面へアクセス' do
         it 'ログイン画面へ遷移し、フラッシュが表示されること' do
           visit new_user_event_path(user_a)
           expect(page).to have_current_path login_path
@@ -15,7 +15,7 @@ RSpec.describe "スケジュール管理機能", type: :system do
         end
       end
 
-      context 'スケジュールの編集画面へアクセス' do
+      context 'カレンダーの編集画面へアクセス' do
         it 'ログイン画面へ遷移し、フラッシュが表示されること' do
           visit edit_user_event_path(user_a, event)
           expect(page).to have_current_path login_path
@@ -34,14 +34,14 @@ RSpec.describe "スケジュール管理機能", type: :system do
       context 'ユーザーAでログインしている場合' do
         let(:login_user) { user_a }
 
-        context 'ユーザーAに紐づくスケジュールの新規登録画面へアクセス' do
+        context 'ユーザーAに紐づくカレンダーの新規登録画面へアクセス' do
           it '正常に遷移すること' do
             visit new_user_event_path(login_user)
             expect(page).to have_current_path new_user_event_path(login_user)
           end
         end
 
-        context 'ユーザーAに紐づくスケジュールの編集画面へアクセス' do
+        context 'ユーザーAに紐づくカレンダーの編集画面へアクセス' do
           it '正常に遷移すること' do
             visit edit_user_event_path(login_user, event)
             expect(page).to have_current_path edit_user_event_path(login_user, event)
@@ -52,14 +52,14 @@ RSpec.describe "スケジュール管理機能", type: :system do
       context 'ユーザーBでログインしている場合' do
         let(:login_user) { user_b }
 
-        context 'ユーザーAに紐づくスケジュールの新規登録画面へアクセス' do
+        context 'ユーザーAに紐づくカレンダーの新規登録画面へアクセス' do
           it 'ホーム画面へ遷移すること' do
             visit new_user_event_path(user_a)
             expect(page).to have_current_path root_path
           end
         end
 
-        context 'ユーザーAに紐づくスケジュールの編集画面へアクセス' do
+        context 'ユーザーAに紐づくカレンダーの編集画面へアクセス' do
           it 'ホーム画面へ遷移すること' do
             visit edit_user_event_path(user_a, event)
             expect(page).to have_current_path root_path
@@ -68,43 +68,51 @@ RSpec.describe "スケジュール管理機能", type: :system do
       end
     end
 
-    # describe '新規登録機能' do
-    #   let(:login_user) { user_a }
+    describe '新規登録機能' do
+      let(:login_user) { user_a }
+      let!(:date) { Time.zone.now.strftime("%Y-%m-%d") }
+      
+      before do
+        # ホーム画面へ遷移する
+        visit root_path
+        # カレンダーからイベントを登録したい日付をクリック
+        page.find("a[href='/users/#{login_user.id}/events/new?default_date=#{date}']").click
+      end
 
-    #   context 'メーカー名を有効な値で登録した場合' do
-    #     it '登録に成功する' do
-    #       visit new_user_maker_path(login_user)
-    #       fill_in 'maker[name]', with: 'createメーカー'
-    #       # DB上に登録されていること
-    #       expect {
-    #         click_button '登録'
-    #       }.to change(Maker, :count).by(1)
-    #       # 一覧画面へ遷移していること
-    #       expect(page).to have_current_path user_makers_path(login_user)
-    #       # 成功時のフラッシュが表示されていること
-    #       expect(page).to have_selector 'div.alert.alert-success'
-    #       # 登録したメーカーが表示されていること
-    #       expect(page).to have_content 'createメーカー'
-
-    #       # 売上登録画面のセレクトボックスにメーカー名が反映されていること
-    #       visit new_user_sale_path(login_user)
-    #       expect(page).to have_content 'createメーカー'
-    #     end
-    #   end
-
-    #   context 'メーカー名を無効な値で登録した場合' do
-    #     it '登録に失敗する' do
-    #       visit new_user_maker_path(login_user)
-    #       fill_in 'maker[name]', with: ''
-    #       # DB上に登録されていないこと
-    #       expect {
-    #         click_button '登録'
-    #       }.to_not change(Maker, :count)
-    #       # エラーメッセージが表示されていること
-    #       expect(page).to have_selector 'div.alert.alert-danger'
-    #     end
-    #   end
-    # end
+      context 'イベントを有効な値で登録した場合' do
+        it '登録に成功する' do
+          # 日付選択のセレクトボックスがデフォルトでクリックした日付になっていること
+          expect(page).to have_select('event[start_time(1i)]', selected: date[0..3])
+          expect(page).to have_select('event[start_time(2i)]', selected: date[5..6].to_i.to_s)
+          expect(page).to have_select('event[start_time(3i)]', selected: date[8..9].to_i.to_s)
+          expect(page).to have_select('event[start_time(4i)]', selected: '00')
+          expect(page).to have_select('event[start_time(5i)]', selected: '00')
+          fill_in 'event[title]', with: 'createイベント'
+          # # DB上に登録されていること
+          expect {
+            click_button 'カレンダー登録'
+          }.to change(Event, :count).by(1)
+          # ホーム画面へ遷移していること
+          expect(page).to have_current_path root_path
+          # 成功時のフラッシュが表示されていること
+          expect(page).to have_selector 'div.alert.alert-success'
+          # 登録したイベントが表示されていること
+          expect(page).to have_content 'createイベント'
+        end
+      end
+      
+      context 'イベントを無効な値で登録した場合' do
+        it '登録に失敗する' do
+          fill_in 'event[title]', with: ''
+          # DB上に登録されていないこと
+          expect {
+            click_button 'カレンダー登録'
+          }.to_not change(Event, :count)
+          # エラーメッセージが表示されていること
+          expect(page).to have_selector '#error_explanation'
+        end
+      end
+    end
 
 
     # describe '編集機能' do
