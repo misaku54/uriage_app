2f12b94a300a142389089c2c7e2dc63725cfe1e1
misaku54 <eisei0504@outlook.jp>
2023-09-04 20:59:45 +0900

    [fix]変数の配置変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
     return unless logged_in?
+    today   = Time.zone.now
+    
     @user   = current_user
     @events = @user.events
-    today   = Time.zone.now
-
     @sales_total_amount = @user.sales_sum(today)
     @sales_trend = @user.hourly_sales_sum(today)
   end
