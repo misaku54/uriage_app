bfdd7e532f8437057c1f6d7649420119b9243819
misaku54 <eisei0504@outlook.jp>
2023-09-07 20:30:53 +0900

    [fix]ハンバーガーメニューロジックををログアウト時には読み込まないように修正

--- app/javascript/custom/toggle.js
+++ app/javascript/custom/toggle.js
@@ -1,6 +1,8 @@
 document.addEventListener('turbo:load', () => {
   const humbager   = document.querySelector('.humbager');
   const toggleMenu = document.querySelector('.toggle-menu');
+  
+  if(!humbager){ return } 
   humbager.addEventListener('click', () => {
     toggleMenu.classList.toggle('open');
   })
