ce79f5f6a9b79d89d30f60984b9331e65e33a143
misaku54 <eisei0504@outlook.jp>
2023-09-23 10:09:14 +0900

    [fix]日時集計画面のバグの一時対応

--- app/javascript/aggregates/form.js
+++ app/javascript/aggregates/form.js
@@ -1,19 +1,19 @@
-import { checkTime, btnDisabled } from 'module/validation';
+// import { checkTime, btnDisabled } from 'module/validation';
 
-document.addEventListener('turbo:load', () => {
-  const inputs = document.querySelectorAll('input[name^="search_daily["]');
-  const parent = inputs[0].closest('.form_group');
+// document.addEventListener('turbo:load', () => {
+//   const inputs = document.querySelectorAll('input[name^="search_daily["]');
+//   const parent = inputs[0].closest('.form_group');
 
-  // inputのイベント設定
-  inputs.forEach((input) => {
-    input.addEventListener('input', () => {
-      const startInput = document.querySelector('input[name="search_daily[start_date]"]');
-      const endInput   = document.querySelector('input[name="search_daily[end_date]"]');
-      const startDate = new Date(startInput.value);
-      const endDate   = new Date(endInput.value);
+//   // inputのイベント設定
+//   inputs.forEach((input) => {
+//     input.addEventListener('input', () => {
+//       const startInput = document.querySelector('input[name="search_daily[start_date]"]');
+//       const endInput   = document.querySelector('input[name="search_daily[end_date]"]');
+//       const startDate = new Date(startInput.value);
+//       const endDate   = new Date(endInput.value);
 
-      checkTime(startDate, endDate, parent);
-      btnDisabled();
-    });
-  })
-})
+//       checkTime(startDate, endDate, parent);
+//       btnDisabled();
+//     });
+//   })
+// })
